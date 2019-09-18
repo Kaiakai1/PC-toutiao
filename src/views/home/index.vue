de5ffe45b9a5d821e0bbf1699936e31255f94c0a
yyc <1439853755@qq.com>
2019-09-18 19:41:40 +0800

    '完成了主页侧边导航栏'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>我是主页</div>
+  <!-- container布局 -->
+  <!-- 只能是 <el-container>包裹 -->
+  <el-container>
+    <!-- 左侧导航 -->
+    <el-aside width="200px">
+      <layout-aside ></layout-aside>
+    </el-aside>
+    <!-- 右侧内容 -->
+  <el-container>
+    <!-- 头部 -->
+    <el-header>头部</el-header>
+    <router-view>zhongjian </router-view>
+  </el-container>
+  </el-container>
 </template>
 
 <script>
+// 引入左侧导航栏组件
+import layoutAside from '../../components/layoutAside/layout-aside'
 export default {
-
+  // 注册组件
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
